da5e95d6042eaa12a89b66ac345eaf500b780018
Lucas <luks_soares94@hotmail.com>
2024-04-25 12:12:13 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(texto){
+    texto = texto.replace(/\s/g, '').toLowerCase();
+    let inicio = 0;
+    let fim = texto.length - 1;
+
+    for (inicio = 0; inicio < fim; inicio++, fim--){
+        if (texto[inicio] !== texto[fim]){
+            return false;
+        }
+    }
+    return true;
 }
 
+
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let menor = arr[0];
+    let maior = arr[0];
+
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] < menor){
+            menor = arr[i];
+        }
+        if (arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return [menor, maior];
 }
